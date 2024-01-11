aa1e482c43f81165a57c415da23c0516353134ff
Ravindra <ravindra4mahajan@gmail.com>
2024-01-11 10:03:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npx create-react-app crud
+npm i axios
+npm i react-router-dom
+import the CSS CDN of bootstrap 5 to make the application simple
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Create />}></Route>
+          <Route path="/read" element={<Read />}></Route>
+          <Route path="/update" element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
